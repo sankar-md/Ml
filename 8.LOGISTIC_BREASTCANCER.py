00822e937aa1981a218e104540978364e773409e
sankar-md <112231837+sankar-md@users.noreply.github.com>
2024-01-23 11:54:23 +0530

    Add files via upload

---
+++ 8.LOGISTIC_BREASTCANCER.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 12:16:27 2023
+
+@author: GEOMOL GEORGE
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 11:53:58 2023
+
+@author: GEOMOL GEORGE
+"""
+
+import numpy as np
+import pandas as pd
+
+#"Importing the dataset
+
+
+
+# divide the dataset into concepts and targets. Store the concepts into X and targets into y.
+dataset = pd.read_csv("D:/GEO/BE COURSES/2022 dec/LAB/DATASET/breastcancer.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+#Splitting the dataset into the Training set and Test  
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 2)
+
+#Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+#Logistic Regression (LR) classifier model
+
+#Display the results (confusion matrix and accuracy)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print('Accuracy Score:confusion matrix')
+accuracy_score(y_test, y_pred)
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
